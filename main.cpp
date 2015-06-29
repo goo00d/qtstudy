663db339473b4e847c4bf5c64bb4c49d653014e4
goo00d <goo00d123@gmail.com>
2015-06-30 00:01:34 +0800

    new test

--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QWidget>
+#include <QSpinBox>
+#include <QSlider>
+#include <QHBoxLayout>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+        QApplication app(argc, argv);
+        QWidget *window = new QWidget;
+        window->setWindowTitle("Enter your age");
 
-    return a.exec();
+        QSpinBox *spinBox = new QSpinBox;
+        QSlider *slider = new QSlider(Qt::Horizontal);
+        spinBox->setRange(0, 130);
+        slider->setRange(0, 130);
+
+        QObject::connect(slider, SIGNAL(valueChanged(int)), spinBox, SLOT(setValue(int)));
+        QObject::connect(spinBox, SIGNAL(valueChanged(int)), slider, SLOT(setValue(int)));
+        spinBox->setValue(35);
+
+        QHBoxLayout *layout = new QHBoxLayout;
+        layout->addWidget(spinBox);
+        layout->addWidget(slider);
+        window->setLayout(layout);
+
+        window->show();
+
+        return app.exec();
 }
---
+++ testexcel/testexcel.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-06-30T00:00:16
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = testexcel
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
